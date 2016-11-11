e8b2342d109c48459ec032a6e3710fc23dc1a863
JoeFannie <865365468@qq.com>
2016-11-11 17:02:13 +0800

    Update VisualizationLoss.m
    support multi-file visualization

--- Evaluation/VisualizationLoss.m
+++ Evaluation/VisualizationLoss.m
@@ -1,51 +1,38 @@
-train_file = fopen('../attr_id/log/v4_bn.log.train', 'r');
-train_log = textscan(train_file, '%d %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f');
-only_file = fopen('../attr_id/log/only.log.train', 'r');
-only_log = textscan(only_file, '%d %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f');
-x = only_log{1};
-
-for i = 1 : 40
-loss_train = train_log{i+4};
-loss_only = only_log{i+4};
-% for i = 1 : 39
-%    loss_train = train_log{i+5} + loss_train; 
-% end
-% for i = 1 : 39
-%    loss_only = only_log{i+5} + loss_only; 
-% end
-[x_sample, loss_train] = filtData(x, loss_train(1:size(x,1)), 16);
-[~, loss_only] = filtData(x, loss_only, 16);
-
-plot(x_sample, loss_train, 'r');
+function [ flag ] = VisualizeLoss( varargin )
+%UNTITLED10 此处显示有关此函数的摘要
+%   此处显示详细说明
+N = nargin-1;
+color_table = ['r', 'g', 'b', 'k', 'y'];
+sample_rate = varargin{end};
+figure;
 hold on;
-plot(x_sample, loss_only, 'g');
-hold off;
-disp(num2str(i));
-pause;
+for i = 1 : N
+    file = fopen(varargin{i}, 'r');
+    line = strsplit(fgets(file));
+    switch length(line)
+        case 45
+            log = textscan(file, '%d %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f');
+        case 46
+            log = textscan(file, '%d %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f');
+            [~, loss_mid] = filtData(log{1}, log{end}, sample_rate);
+        case 47
+            log = textscan(file, '%d %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f');
+            [~, loss_mid] = filtData(log{1}, log{end-1}, sample_rate);
+    end
+    loss_attr = log{5};
+    for j = 1 : 39
+       loss_attr = loss_attr + log{j+5}; 
+    end
+    [x_sample, loss_attr] = filtData(log{1}, loss_attr, sample_rate);
+    if(length(line) >= 46)
+%         [AX, H1, H2] = plotyy(x_sample, loss_attr, x_sample, loss_mid);
+%         set(H1, 'Linestyle', '-', 'color', color_table(mod(i, 5)), 'Linewidth', 2.5);
+%         set(H2, 'Linestyle', '--', 'color', color_table(mod(i, 5)), 'Linewidth', 2.5);
+        plot(x_sample, loss_mid, 'Linestyle', '--', 'color', color_table(mod(i, 5)), 'Linewidth', 2.5);
+    end
+    plot(x_sample, loss_attr, 'Linestyle', '-', 'color', color_table(mod(i, 5)), 'Linewidth', 2.5);
+    fclose all;
 end
-% axis([x(1), x(end), 10, 20]);
+hold off;
+end
 
-% train_file = fopen('train_shuffle.txt', 'r');
-% new_file = fopen('train_shuffle_new.txt', 'w');
-% train_list = textscan(train_file, '%s %d');
-% attr_train_new = zeros(35996, 80);
-% names = train_list{1};
-% ids = train_list{2};
-% overlap_id = [];
-% count = 0;
-% j = 1;
-% for i = 1 : length(names)
-%    id = str2double(names{i}(1:end-4));
-%    if(id >= 184885)
-%       overlap_id(end+1) = id;
-%       count = count + 1;
-%    else
-%       fprintf(new_file, '%s %d\n', names{i}, ids(i));
-%       attr_train_new(j, :) = attr_train(i, :);
-%       j = j + 1;
-%    end
-% end
-% 
-% disp(num2str(count));
-% save attr_train_new.mat attr_train_new;
-fclose all;
