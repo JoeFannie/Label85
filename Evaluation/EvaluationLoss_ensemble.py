3cb7dfba46bda9f3a17bd48fd9b19e82c4c0c8aa
JoeFannie <865365468@qq.com>
2016-11-28 13:08:26 +0800

    Update EvaluationLoss_ensemble.py

--- Evaluation/EvaluationLoss_ensemble.py
+++ Evaluation/EvaluationLoss_ensemble.py
@@ -1,4 +1,4 @@
-caffe_root = '/home/zhangyuan/caojiajiong/caffe-HIK/'
+caffe_root = '/home/zhangyuan/caojiajiong/caffe-HIK-crop/'
 import sys
 sys.path.insert(0, caffe_root+'python')
 import caffe
@@ -28,8 +28,9 @@ loss_list = ['loss_5_o_Clock_Shadow', 'loss_Arched_Eyebrows', 'loss_Attractive',
 'loss_Straight_Hair', 'loss_Wavy_Hair', 'loss_Wearing_Earrings', 'loss_Wearing_Hat', \
 'loss_Wearing_Lipstick', 'loss_Wearing_Necklace', 'loss_Wearing_Necktie', 'loss_Young']
 
-file = open('accuracy_ensemble_9_96_128.txt', 'a')
-for i in range(0, 4):
+file = open('accuracy_ensemble_branch_conv5_fusion_wh124_7W.txt', 'a')
+feat_file = open('feat_branch_conv5_fusion_wh124_7W.txt', 'w')
+for i in range(0, 1):
 	loss_count = 0;
 	accuracy = np.zeros((41))
 	if i >= 1:
@@ -37,10 +38,11 @@ for i in range(0, 4):
 	file.writelines(str(i*10000+60000)+'\n')
 	for loss in loss_list:
 	    # ensemble number
-		N = 9
+		feat_file.writelines(loss+'\n')
+		N = 10
 		data = np.zeros((20000, 2))
 		for n in range(0, N):
-			dir_name = '/home/zhangyuan/caojiajiong/attribute_learning/features_all/widder_v1_wh' + str(96 + n*4) + '_' + str(i*10000+60000) + '/'
+			dir_name = '/home/zhangyuan/caojiajiong/attribute_learning/features_all/branch_conv5_fusion_wh124_7W_' + str(1 + n*1) + '/'
 			loss_name = dir_name + loss
 			#save_name = dir_name + loss + '.txt'
 			feature = lmdb.open(loss_name)
@@ -53,6 +55,8 @@ for i in range(0, 4):
 			  datum.ParseFromString(value)
 			  data[count, :] = data[count, :] + caffe.io.datum_to_array(datum).reshape(2)
 			  count = count + 1
+		for k in range(0, 19963):
+			feat_file.writelines(str(data[k, 0]) + ' ' + str(data[k, 1]) + '\n')
 		data[0:-1, :] = data[0:-1, :] > 0.5 * N
 		data = data.astype(float)
 		bool_matrix = data[0:19962, 0] == data_label[0:19962, loss_count*2]
@@ -65,6 +69,7 @@ for i in range(0, 4):
 	for j in range(0, 41):
 		file.writelines(str(accuracy[j])+'\n')
 file.close()
+feat_file.close()
 	#accuracy_name = dir_name + 'accuracy.txt'
 	#np.savetxt(accuracy_name, accuracy, delimiter = ' ')
 	
